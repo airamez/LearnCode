3715eff1de6a4be6d0506aa886b881e3029ffb4e
Jose Santos <airamez@gmail.com>
2016-05-28 20:04:07 -0700

    Refactory and addition of the login/register page

---
+++ WebApp/LearnCode.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="LearnCode.asmx.cs" Class="WebApp.WebService1" %>
---
+++ WebApp/LearnCode.asmx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApp {
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class LearnCode : System.Web.Services.WebService {
+
+        [WebMethod]
+        public string HelloWorld() {
+            return "Hello World";
+        }
+    }
+}
